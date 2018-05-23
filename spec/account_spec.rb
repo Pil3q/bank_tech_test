89b7c95750c565cab798cbb17b920612a6b96d96
Patryk Pilecki <ppilecki@icloud.com>
2018-05-23 14:09:42 +0100

    additional account test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -12,6 +12,12 @@ describe Account do
     subject.withdraw(500)
     expect(subject.balance).to eq 500
   end
+  it 'will has transaction history with the dates, and types of transactions' do
+    date = "2018-05-20"
+    subject.pay_in(1000, date)
+    subject.withdraw(500, date)
+    expect(subject.history).to include({ amount: 1000, date: "#{date}", type: :credit, balance: 1000 }, { amount: 500, date: "#{date}", type: :debit, balance: 500 })
+  end
   it 'throws an error while trying to withdraw from empty account' do
     expect { subject.withdraw(500) }.to raise_error 'You are broke, maybe a loan?'
   end
