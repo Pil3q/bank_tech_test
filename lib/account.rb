210f5a2bdf84ae789ab4d101742b6bf13dfb932f
Patryk Pilecki <ppilecki@icloud.com>
2018-05-23 09:57:11 +0100

    statement printer/with date and type of transaction/ error while trying to withdraw mor than on acc

--- lib/account.rb
+++ lib/account.rb
@@ -2,13 +2,23 @@ class Account
   attr_reader :balance
   def initialize
     @balance = 0
+    @history = []
   end
 
   def pay_in(amount)
+    date = Time.new.strftime("%Y-%m-%d")
     @balance += amount
+    @history << { :amount => amount, :date => date, :type => :credit, :balance => @balance }
   end
 
   def withdraw(amount)
+    raise "You are broken, maybe a loan?" if amount > @balance
+    date = Time.new.strftime("%Y-%m-%d")
     @balance -= amount
+    @history << { :amount => amount, :date => date, :type => :debit, :balance => @balance }
+  end
+
+  def print_statement
+    @history.each { |transaction| puts "Type: #{transaction[:type]}, amount: £#{transaction[:amount]} at #{transaction[:date]}, balance £#{transaction[:balance]}"}
   end
 end
