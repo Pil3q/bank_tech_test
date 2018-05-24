67e5e242f512fa7accab51a6a269b04f537689f9
Patryk Pilecki <ppilecki@icloud.com>
2018-05-24 20:24:52 +0100

    statement printer test refactor

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -1,12 +1,14 @@
 require 'statement_printer'
 describe Statement do
-  date = "20/05/2018"
-  let(:transaction_1) { double(:transaction_1, date: date, credit: 1000, debit: nil, balance: 1000) }
-  let(:transaction_2) { double(:transaction_1, date: date, credit: nil, debit: 500, balance: 1000) }
-  let(:history) { double(:history, transactions: [transaction_1, transaction_2]) }
-  let(:account) { double(:account, history: history) }
+  describe '#print' do
+    date = "20/05/2018"
+    let(:transaction_1) { double(:transaction_1, date: date, credit: 1000, debit: nil, balance: 1000) }
+    let(:transaction_2) { double(:transaction_1, date: date, credit: nil, debit: 500, balance: 1000) }
+    let(:history) { double(:history, transactions: [transaction_1, transaction_2]) }
+    let(:account) { double(:account, history: history) }
 
-  it 'will print statement with the dates, and types of transactions' do
-    expect { Statement.print(account) }.to output("date || credit || debit || balance\n20/05/2018 ||  || 500.00 || 1000.00\n20/05/2018 || 1000.00 ||  || 1000.00\n").to_stdout
+    it 'will print statement with the dates, and types of transactions' do
+      expect { Statement.print(account) }.to output("date || credit || debit || balance\n20/05/2018 ||  || 500.00 || 1000.00\n20/05/2018 || 1000.00 ||  || 1000.00\n").to_stdout
+    end
   end
 end
